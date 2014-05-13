05aefd92c63ad689e64f76e37a79e712a902a29b
Rocky Bernstein <rocky@cpan.org>
2014-05-13 02:56:51 -0400

    Get ready for release 1.31

--- lib/Term/ReadLine/Perl5.pm
+++ lib/Term/ReadLine/Perl5.pm
@@ -4,7 +4,7 @@ package Term::ReadLine::Perl5;
 use warnings; use strict;
 no warnings 'once';
 
-our $VERSION = '1.30';
+our $VERSION = '1.31';
 
 use Carp;
 use Term::ReadLine::Perl5::History;
--- lib/Term/ReadLine/Perl5/readline.pm
+++ lib/Term/ReadLine/Perl5/readline.pm
@@ -17,7 +17,7 @@ use File::Glob ':glob';
 
 # no critic
 # Version might be below Perl5.pm
-our $VERSION = '1.30';
+our $VERSION = '1.31';
 
 #
 # Separation into my and vars needs more work.
@@ -3291,7 +3291,6 @@ completion to continue.
 sub rl_filename_list_deprecated
 {
     my $pattern = $_[0];
-    my @glob_expand = bsd_glob($pattern);
     my @files = (<$pattern*>);
     if ($var_CompleteAddsuffix) {
         foreach (@files) {
